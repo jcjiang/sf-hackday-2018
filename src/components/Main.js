0e28a60340482c9ac99b512c9a0694e472010139
Ariel Tam <ariel.tam@salesforce.com>
2018-08-07 16:05:02 -0400

    RNG YEEEEE

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 
 const Main = styled.div`
   display: flex;
-  width: 100vw;
+  width: 70vw;
   height: 100vh;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding-bottom: 20vh;
+  text-align: center;
+  margin-left: 15%;
 `;
 
 export default Main;
--- src/containers/Presenter.js
+++ src/containers/Presenter.js
@@ -8,11 +8,19 @@ export default class Presenter extends Component {
         super(props);
     }
     
+    makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < 6; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    }
+
     render() {
         return (
             <Main>
                 <Title description="Share this meeting code with your viewers.">
-                    <Code random="QWERTY"></Code>
+                    <Code random={this.makeid()}></Code>
                     <Button 
                         name="START RECORDING" 
                         link="/"
