fac80c926e2f03dd2477c2b810a0b429949031e4
Jiachen Jiang <jiachen.jiang@jiachenjia-ltm.internal.salesforce.com>
2018-08-07 15:53:59 -0400

    stopwatch shenanigans

--- src/containers/Viewer.js
+++ src/containers/Viewer.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button } from '../components';
 
 export default class Viewer extends Component {
-    
+
     constructor(props) {
         super(props);
+        this.state = {
+          status: false,
+          runningTime: 0
+        };
     }
-    
+
     render() {
+        const { status, runningTime } = this.state;
         return (
             <div>
                 <h2>Viewer</h2>
+                <Button name="Presenter" link="/presenter" type="Primary"/>
+               />,
             </div>
         )
     }
-}
+  }
