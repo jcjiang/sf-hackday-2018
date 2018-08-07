e266929a3283be42fcd05ddc11ca48a784b75b89
vritant <vritant.bhardwaj@demandware.com>
2018-08-07 12:06:22 -0400

    example button

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+    background: #222;
+    color: #eee;
+`;
+
+export default function Button(props) {
+    return <PrimaryButton> Button </PrimaryButton>
+}
