6a901893a43a35708448c48a517c463f8d9aa958
vritant <vritant.bhardwaj@demandware.com>
2018-08-07 21:48:11 -0400

    removed warnings

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,15 @@
-import React, {Component}   from 'react';
-import styled               from 'styled-components';
+import React   from 'react';
 
 import {Button, 
         Title,
         Main }             from '../components';
 
-export default class Home extends Component {
-    
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <Main>
-                <Title description="Join a Meeting!"/>
-                <Button name="PRESENTER" link="/presenter" type="Primary"/>
-                <Button name="VIEWER" link="/viewer" type="Secondary"/>
-            </Main>
-        )
-    }
+export default function Home(props) {
+    return (
+        <Main>
+            <Title description="Join a Meeting!"/>
+            <Button name="PRESENTER" link="/presenter" type="Primary"/>
+            <Button name="VIEWER" link="/viewer" type="Secondary"/>
+        </Main>
+    );
 }
