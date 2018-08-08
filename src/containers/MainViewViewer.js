f07b323b2d0029de3659009db70e382cf64d1546
Saatchi Bhalla <saatchi.bhalla@saatchibha-ltm.internal.salesforce.com>
2018-08-07 21:22:15 -0400

    mic and speech rec

--- src/containers/MainViewViewer.js
+++ src/containers/MainViewViewer.js
@@ -8,29 +8,55 @@ export default class MainViewViewer extends Component {
         super(props);
         this.state = {
           record: false,
-          save: false
-        }
-    }
+          save: false,
+          transcription: ""
+        };
 
-    onSave(recordedBlob) {
-      console.log("recordedBlob");
     }
 
     onStop(recordedBlob) {
-      console.log(recordedBlob.blobURL);
+      /*console.log(recordedBlob.blobURL);
       const audio = new Audio(recordedBlob.blobURL);
       console.log(audio);
-      audio.play();
+      audio.play();*/
     }
 
     startRecording = () => {
       this.setState({
         record: true
       });
+
+      var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+      console.log(recognition);
+      recognition.onstart = function() {
+        alert('Voice recognition activated. Try speaking into the microphone.');
+      }
+
+      recognition.onspeechend = function() {
+        alert('You were quiet for a while so voice recognition turned itself off.');
+      }
+
+      recognition.onerror = function(event) {
+        if(event.error == 'no-speech') {
+          alert('No speech was detected. Try again.');
+        };
+      }
+
+      recognition.onresult = function(event) {
+        var current = event.resultIndex;
+
+        var transcript = event.results[current][0].transcript;
+        var addedTranscription = this.state.transcription += transcript + ". "
+        this.setState({
+          transcription: addedTranscription
+        });
+      }
+      recognition.onresult.bind(this);
+      recognition.continuous = true;
+      recognition.start();
     }
 
     stopRecording = () => {
-      console.log("STOP RECORDING");
       this.setState({
         record: false
       });
@@ -44,7 +70,6 @@ export default class MainViewViewer extends Component {
               record={record}         // defaults -> false.  Set to true to begin recording
               save={save}           // set to true if you want to save
               className={"recording"} // css class name
-              onSave={this.onSave}        // callback to save your recording
               onStop={this.onStop}        // callback to execute when audio stops recording
               strokeColor="#000000"     // sound wave color
               backgroundColor="#ffffff"  // background color
