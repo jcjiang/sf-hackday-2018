933f63aa2dde5fc3d050b1e257801b59cbf043aa
Ariel Tam <ariel.tam@salesforce.com>
2018-08-07 23:03:20 -0400

    idk styling

--- src/containers/MainViewViewer.js
+++ src/containers/MainViewViewer.js
@@ -1,8 +1,34 @@
 import React, {Component} from 'react';
 import Header from '../components/Header.js'
 import firebase from '../db/fire';
+import styled from 'styled-components';
 
 let id = 0;
+
+const ReactButton = styled.button`
+    background: #eee;
+    color: #56CCF2;
+    width: 100px;
+    padding: 20px;
+    margin-top: 50px;
+    border: 2px solid #56CCF2;
+    border-radius: 5px;
+    font-family: 'Roboto Mono', monospace;
+    font-size: 1.5rem;
+`;
+
+const Main = styled.div`
+    display: flex;
+    width: 60vw;
+    min-height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20vh;
+    margin: 0 auto;
+    text-align: center;
+    font-family: 'Roboto Mono', monospace;
+    font-size: 1.5rem;
+`;
 
 export default class MainViewViewer extends Component {
 
@@ -44,13 +70,13 @@ export default class MainViewViewer extends Component {
       this.setState(state => {
         if (state.status) {
           this.setState({ timestamps: [...this.state.timestamps, this.state.runningTime]});
-          console.log(this.state.runningTime);
+          //console.log(this.state.runningTime);
           this.itemsRef.push({
             sessionID: 'ABCDEF',
             timestamp: this.state.runningTime,
             key: id++,
           });
-          console.log(this.itemsRef);
+          //console.log(this.itemsRef);
           clearInterval(this.timer);
         } else {
           const startTime = Date.now() - this.state.runningTime;
@@ -64,11 +90,17 @@ export default class MainViewViewer extends Component {
 
     render() {
       const { status, runningTime, timestamps } = this.state;
+
+      const listTimeStamps = this.state.timestamps.map((ts) => <p>{ts}</p>);
+
       return (
         <div>
           <Header name="VIEWER"></Header>
-          <p>{timestamps}</p>
-          <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
+          <Main>
+            <ReactButton onClick={this.handleClick}>{status ? 'Stop' : 'Start'}
+            </ReactButton>
+            <div>{listTimeStamps}</div>
+          </Main>
         </div>
       );
     }
